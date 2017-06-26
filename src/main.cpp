7f8fdda2d37cb92a2466f61e0d17ee9fe413c6b5
Tomáš Báča <bacatoma@fel.cvut.cz>
2017-06-26 12:30:29 +0200

    added camera info and updated dans trajectories

--- src/main.cpp
+++ src/main.cpp
@@ -11,23 +11,23 @@
  */
 class TrajectoryLoader {
 
-public:
+  public:
 
     TrajectoryLoader();                                                         // definition of constructor
 
-private:
+  private:
     mrs_msgs::TrackerTrajectory trajectory_;
     ros::NodeHandle nh_;
-    
+
     // publishers
     ros::ServiceClient service_client_trajectory_;
 
-		bool use_yaw_; // whether to fly trajectory with desired yaw
-		bool fly_now_; // whether to start tracking the trajectory immediately after loading
-		bool loop_;
-		double max_altitude_, min_altitude_;
+    bool use_yaw_; // whether to fly trajectory with desired yaw
+    bool fly_now_; // whether to start tracking the trajectory immediately after loading
+    bool loop_;
+    double max_altitude_, min_altitude_;
     std::string filename_;
-    
+
     // functions
     void publishTrajectory();     
     void loadTrajectoryFromFile();
@@ -38,54 +38,53 @@ private:
  */
 TrajectoryLoader::TrajectoryLoader() {
 
-    nh_ = ros::NodeHandle("~");
+  nh_ = ros::NodeHandle("~");
 
-    // publisher for new odometry
-    service_client_trajectory_ = nh_.serviceClient<mrs_msgs::TrackerTrajectorySrv>("trajectory_topic");
-		
-		// load params
-		use_yaw_ = nh_.param("use_yaw", false);
-		fly_now_ = nh_.param("fly_now", false);
-		loop_ = nh_.param("loop", false);
-		filename_ = nh_.param("filename", std::string());
-		min_altitude_ = nh_.param("min_altitude", 1.0);	
-		max_altitude_ = nh_.param("max_altitude", 20.0);
+  // publisher for new odometry
+  service_client_trajectory_ = nh_.serviceClient<mrs_msgs::TrackerTrajectorySrv>("trajectory_topic");
 
-    if (filename_.empty()){
-        ROS_ERROR("The filename has to be set. \nFor example: rosrun trajectory_handler load_trajectory _filename:=trajectory.txt");
-        ros::shutdown();
-        return;
-    }
+  // load params
+  use_yaw_ = nh_.param("use_yaw", false);
+  fly_now_ = nh_.param("fly_now", false);
+  loop_ = nh_.param("loop", false);
+  filename_ = nh_.param("filename", std::string());
+  min_altitude_ = nh_.param("min_altitude", 1.0);	
+  max_altitude_ = nh_.param("max_altitude", 20.0);
 
+  if (filename_.empty()){
+    ROS_ERROR("The filename has to be set. \nFor example: rosrun trajectory_handler load_trajectory _filename:=trajectory.txt");
+    ros::shutdown();
+    return;
+  }
 
-    loadTrajectoryFromFile();
-    publishTrajectory();
+  loadTrajectoryFromFile();
+  publishTrajectory();
 }
-
 
 /**
  * @brief TrajectoryLoader::publishTrajectory
  */
 void TrajectoryLoader::publishTrajectory() {
+
   // publish plan by service client
   mrs_msgs::TrackerTrajectorySrv srv;
   srv.request.trajectory_msg = trajectory_;
   ROS_INFO("publishing trajectory");
-  if (service_client_trajectory_.call(srv)){
+  if (service_client_trajectory_.call(srv)) {
+
     // if calling was successful (mbzirc_tracker responded)
     if(srv.response.success){
 
-      ROS_INFO("%s",srv.response.message.c_str());
+      ROS_INFO("%s", srv.response.message.c_str());
 
-    }else{
+    } else {
 
-      ROS_ERROR("%s",srv.response.message.c_str());
+      ROS_ERROR("Service, no success: %s", srv.response.message.c_str());
 
     }
-  }else {
-  
-      ROS_ERROR("%s",srv.response.message.c_str());
- 
+  } else {
+
+    ROS_ERROR("Service was not called: %s", srv.response.message.c_str());
   }
 }
 
@@ -93,50 +92,50 @@ void TrajectoryLoader::publishTrajectory() {
  * @brief TrajectoryLoader::loadTrajectoryFromFile
  */
 void TrajectoryLoader::loadTrajectoryFromFile(){
-    
-    std::ifstream in(filename_.c_str(), std::ifstream::in);
-    
-    if (!in) {
 
-       ROS_ERROR("Cannot open %s", filename_.c_str());
-       ros::shutdown();
-       return;
-    
-    } else {
+  std::ifstream in(filename_.c_str(), std::ifstream::in);
 
-      ROS_INFO("Loading from file: %s", filename_.c_str());
-      std::string line;
-      mrs_msgs::TrackerTrajectory new_traj;
+  if (!in) {
 
-      while (getline(in, line)) {
-        mrs_msgs::TrackerPoint point;
-        std::istringstream s(line);
+    ROS_ERROR("Cannot open %s", filename_.c_str());
+    ros::shutdown();
+    return;
 
-        s >> point.x;
-        s >> point.y;
-        s >> point.z;
-        s >> point.yaw;
-        if (point.z > max_altitude_) {
+  } else {
 
-          ROS_WARN("Selected altitude %2.2f saturated to max altitude %2.2f", point.z, max_altitude_);
-          point.z = max_altitude_;
+    ROS_INFO("Loading from file: %s", filename_.c_str());
+    std::string line;
+    mrs_msgs::TrackerTrajectory new_traj;
 
-        } else if (point.z < min_altitude_) {
+    while (getline(in, line)) {
+      mrs_msgs::TrackerPoint point;
+      std::istringstream s(line);
 
-          ROS_WARN("Selected altitude %2.2f saturated to min altitude %2.2f", point.z, min_altitude_);
-          point.z = min_altitude_;
-        }
+      s >> point.x;
+      s >> point.y;
+      s >> point.z;
+      s >> point.yaw;
+      if (point.z > max_altitude_) {
 
-        new_traj.points.push_back(point);
+        ROS_WARN("Selected altitude %2.2f saturated to max altitude %2.2f", point.z, max_altitude_);
+        point.z = max_altitude_;
+
+      } else if (point.z < min_altitude_) {
+
+        ROS_WARN("Selected altitude %2.2f saturated to min altitude %2.2f", point.z, min_altitude_);
+        point.z = min_altitude_;
       }
-      in.close();
 
-      new_traj.header.stamp = ros::Time::now();
-      new_traj.use_yaw = use_yaw_;
-      new_traj.fly_now = fly_now_;
-      new_traj.loop = loop_;
-      trajectory_ = new_traj;
+      new_traj.points.push_back(point);
     }
+    in.close();
+
+    new_traj.header.stamp = ros::Time::now();
+    new_traj.use_yaw = use_yaw_;
+    new_traj.fly_now = fly_now_;
+    new_traj.loop = loop_;
+    trajectory_ = new_traj;
+  }
 }
 
 /**
@@ -147,9 +146,9 @@ void TrajectoryLoader::loadTrajectoryFromFile(){
  */
 int main(int argc, char** argv){
 
-    ros::init(argc, argv, "trajectory_loader");
-    ROS_INFO ("Node initialized.");
+  ros::init(argc, argv, "trajectory_loader");
+  ROS_INFO ("Node initialized.");
 
-    TrajectoryLoader tl;
-    return 0;
+  TrajectoryLoader tl;
+  return 0;
 };
